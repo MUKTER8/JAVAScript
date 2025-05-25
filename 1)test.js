62f6a479af803991f1eb192115ff5e09a6131396
MUKTER HOSAIN <mokterprodhan8@gmail.com>
2025-05-25 17:58:49 +0600

    update

---
+++ 1)test.js
@@ -0,0 +1,22 @@
+{
+  let n1 = 20;
+  let n2 = 25;
+  let total = n1 + n2;
+  console.log(total);
+}
+{
+  let n1 = 30;
+  let n2 = 35;
+  let total = n1 + n2;
+  console.log(total);
+}
+
+const student = {
+  name: "John",
+  age: 20,
+  grade: "A",
+  isPass: true,
+};
+student["name"] = "Mukter"; // Using bracket notation to change the name
+console.log(student["name"]); // John
+console.log(student.age); // 20
---
+++ 1.1)practice.js
@@ -0,0 +1,19 @@
+// create a const object called "product" to store information shown in picture.
+const product = {
+  title: "CT Ball pen",
+  color: "Black",
+  rating: 4.5,
+  price: 270,
+  offer: "5% off",
+  description: "This is a very good quality ball pen.",
+};
+console.log(product);
+// Accessing the properties of the product object
+const profile = {
+  username: "Mukter Hosain",
+  email: "mokterprodhan8@gmail.com",
+  isFollow: false,
+  followers: 1000,
+  following: 500,
+};
+console.log(profile);
